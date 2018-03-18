4c32c8290f56e6ec0e8cc17a0383a7922b365804
Luis Montealegre <montealegreluis@gmail.com>
2018-03-18 11:01:35 -0600

    chore: Add mongoose configuration for the Store model

--- start.js
+++ start.js
@@ -2,6 +2,15 @@ require('dotenv').config({ path: '.env' });
 
 const app = require('./app');
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error) => {
+    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${error.message}`);
+});
+
+require('./models/store')
+
 app.set('port', process.env.PORT || 7777);
 
 const server = app.listen(app.get('port'), () => {
