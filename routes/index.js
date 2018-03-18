467cea26358be65f1ea31de8dc0daad132d7369b
Luis Montealegre <montealegreluis@gmail.com>
2018-03-18 09:50:36 -0600

    chore: Configure pug and add initial view and layout

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    response.send('Hey! It works!');
+    response.render('hello');
 });
 
 module.exports = router;
