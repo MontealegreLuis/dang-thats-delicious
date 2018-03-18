0586852b3a6fb6551c6abec9652cf1b9426d6281
Luis Montealegre <montealegreluis@gmail.com>
2018-03-18 10:12:23 -0600

    chore: Add view helpers configuration file

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
 
+const helpers = require('./helpers');
 const routes = require('./routes/index');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// pass variables to our templates + all requests
+app.use((req, res, next) => {
+    res.locals.h = helpers;
+    next();
+});
 
 app.use('/', routes);
 
---
+++ helpers.js
@@ -0,0 +1,1 @@
+exports.siteName = `Now That's Delicious!`;
